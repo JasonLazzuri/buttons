4f825c02fa0edcc6af8fa4fba7c76b08a3072b7e
bl and jl <bl@>
2017-03-21 15:54:00 -0700

    add .before() DOM manipulation

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
   $("#hello").click(function(){
     $("#user").prepend("<li>Hello!</li>");
     $("#webpage").prepend("<li>Why hello there!</li>");
+    $("body").removeClass();
+    $("body").addClass("background-color2");
     $("ul#user").children("li").first().click(function() {
       $(this).remove();
       });
@@ -13,6 +15,8 @@ $(document).ready(function(){
   $("#goodbye").click(function(){
     $("#user").prepend("<li>Goodbye!</li>");
     $("#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("body").removeClass();
+    $("body").addClass("background-color1");
     $("ul#user").children("li").first().click(function() {
       $(this).remove();
       });
@@ -22,13 +26,24 @@ $(document).ready(function(){
   });
 
   $("#stop").click(function(){
-    $("#user").prepend("<li>Stop copying me!</li>")
+    $("#user").prepend("<li>Stop copying me!</li>");
     $("#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("body").removeClass();
+    $("body").addClass("background-color");
     $("ul#user").children("li").first().click(function() {
       $(this).remove();
-      });
+    });
     $("ul#webpage").children("li").first().click(function() {
         $(this).remove();
     });
   });
+
+  $("#dog").click(function(){
+    $("#bark").after("bark ");
+  });
+
+  $("#me").click(function(){
+    $("#me").before("<img src='https://www.what-dog.net/Images/faces2/scroll000.jpg'>")
+  });
+
 });
